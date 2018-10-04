3c12e93bf592df298ae95dcf6835f16fc9d28d39
Pat <me@onpaws.com>
2018-10-04 19:36:38 -0300

    registration, login, and cookie-based auth

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,30 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 import { ApolloServer } from 'apollo-server-express';
-import {User} from "./entity/User";
+import * as express from 'express';
+import * as session from 'express-session';
+import { typeDefs } from './typeDefs';
+import { resolvers } from "./resolvers";
 
-createConnection().then(async connection => {
+const startServer = async () => {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: ({req}: any) => ({req})
+    });
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-    
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-     
-    console.log("Here you can setup and run express/koa/any other framework.");
-    
-}).catch(error => console.log(error));
+    await createConnection();
+    const app = express();
+    app.use(session({
+        secret: 'aoiwutohg',
+        resave: false,
+        saveUninitialized: false,
+    }))
 
+    server.applyMiddleware({ app }); // app is from an existing express app
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+    app.listen({ port: 4000 }, () =>
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+    )
+}
 
-server.applyMiddleware({ app }); // app is from an existing express app
-
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-)
+startServer();
