271eeff3b841a9ddc8a0cde0184826e72f2b2a26
Pat <me@onpaws.com>
2021-01-17 04:04:08 +0100

    add note about why middleware/index.ts was created (for dokku)

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -4,3 +4,6 @@ import { installCookieJWT } from './installCookieJWT.js'
 import { installFrontendProxy } from './installFrontendProxy.js';
 
 export { installDatabasePools, installPostGraphile, installCookieJWT, installFrontendProxy }
+
+// Jan 2021: this file's existence is to fix Dokku 'slimming' the slug by default
+// TODO: consider ways to exclude http-proxy in prod
